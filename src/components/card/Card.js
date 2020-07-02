dda3523d8d5b809c28e43c7bdd8d213c012f3af7
lagbullet <lagbullet@gmail.com>
2020-07-02 12:05:48 +0300

    add index.js to components, fix code style, change class to className in header

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './Card.scss';
 
 class Card extends React.Component {
-
   render() {
-      const {className, caption, data} = this.props;
-      return (
-        <div className={className}>
-            <div className="caption">{caption}</div>
-            <div>{data}</div>
-        </div>
-      );
+    const { caption, children } = this.props;
+    return (
+      <div className="card">
+        <div className="caption">{caption}</div>
+        <div className="text">{children}</div>
+      </div>
+    );
   }
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,12 +2,9 @@ import React from 'react';
 import './Header.scss';
 
 class Header extends React.Component {
-
   render() {
-      const {className, text} = this.props;
-      return (
-        <header class={className}>{text}</header>
-      );
+    const { children } = this.props;
+    return <header className="header">{children}</header>;
   }
 }
 
