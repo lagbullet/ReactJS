8a0fe04a9199c402e45a38bcbdf98b447a553be4
lagbullet <lagbullet@gmail.com>
2020-07-02 15:47:50 +0300

    add checkbox in card component

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './Card.scss';
 
 class Card extends React.Component {
+  state = {
+    color: 'green',
+  };
+
   render() {
     const { caption, children } = this.props;
     return (
-      <div className="card">
-        <div className="caption">{caption}</div>
+      <div className={this.state.color + ' card '}>
+        <div className="captionWrapper">
+          <div className="caption">{caption}</div>
+          <input className="checkbox" type="checkbox" onChange={this.toggleCheckboxChange} />
+        </div>
         <div className="text">{children}</div>
       </div>
     );
   }
+
+  toggleCheckboxChange = () =>
+    this.setState(({ color }) => ({ color: color === 'green' ? 'red' : 'green' }));
 }
 
 export default Card;
