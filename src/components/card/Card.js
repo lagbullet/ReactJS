9f75b2d818857718d66c41683d1c93e0460ef680
lagbullet <40629577+lagbullet@users.noreply.github.com>
2020-07-06 11:38:11 +0300

    Task2: add checkbox in card component (#2)
    * add checkbox in card component
    
    * add .gitignore
    
    * change state object & format

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import classNames from 'classnames';
 import './Card.scss';
 
 class Card extends React.Component {
+  state = {
+    checked: false,
+  };
+
+  toggleCheckboxChange = () => this.setState(({ checked }) => ({ checked: !checked }));
+
   render() {
     const { caption, children } = this.props;
+    const cardClass = classNames({
+      card: true,
+      redCard: this.state.checked,
+      greenCard: !this.state.checked,
+    });
     return (
-      <div className="card">
-        <div className="caption">{caption}</div>
+      <div className={cardClass}>
+        <div className="captionWrapper">
+          <div className="caption">{caption}</div>
+          <input className="checkbox" type="checkbox" onChange={this.toggleCheckboxChange} />
+        </div>
         <div className="text">{children}</div>
       </div>
     );
