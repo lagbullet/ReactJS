06a56bf1a91bba0ce8d14ecb632583203411f22d
lagbullet <40629577+lagbullet@users.noreply.github.com>
2020-07-24 11:18:42 +0300

    Task12: add validation for SignIn component (#12)
    * add validation for SignIn component
    
    * change validation mechanism
    
    * move validation to another module
    
    * fix required function

---
+++ src/components/input/Input.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+class Input extends React.Component {
+  onChange = ({ target: { value } }) => {
+    const { validation } = this.props;
+    let result;
+    for (let i = 0; i < validation.length; i++) {
+      result = this.props.validation[i](value);
+      if (!result) {
+        break;
+      }
+    }
+    this.props.onValid(result);
+  };
+
+  render() {
+    const { label, type, className, placeholder } = this.props;
+    return (
+      <React.Fragment>
+        <label>{label}</label>
+        <input {...{ type, className, placeholder }} onChange={this.onChange} />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Input;
--- src/components/sign-in/SignIn.js
+++ src/components/sign-in/SignIn.js
@@ -1,32 +1,68 @@
 import React from 'react';
+import Input from '../input';
+import { required, isValidEmail, isValidPassword } from '../../validation';
 
-const SignIn = () => (
-  <form>
-    <h3>Sign In</h3>
+class SignIn extends React.Component {
+  state = {
+    validEmail: false,
+    validPassword: false,
+  };
 
-    <div className="form-group">
-      <label>Username</label>
-      <input type="username" className="form-control" placeholder="Enter username" />
-    </div>
+  validEmail = (value) => {
+    this.setState(() => ({ validEmail: value }));
+  };
 
-    <div className="form-group">
-      <label>Password</label>
-      <input type="password" className="form-control" placeholder="Enter password" />
-    </div>
+  validPassword = (value) => {
+    this.setState(() => ({ validPassword: value }));
+  };
 
-    <div className="form-group">
-      <div className="custom-control custom-checkbox">
-        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-        <label className="custom-control-label" htmlFor="customCheck1">
-          Remember me
-        </label>
-      </div>
-    </div>
+  render() {
+    const { validEmail, validPassword } = this.state;
+    return (
+      <form>
+        <h3>Sign In</h3>
 
-    <button type="submit" className="btn btn-primary btn-block">
-      Submit
-    </button>
-  </form>
-);
+        <div className="form-group">
+          <Input
+            type="email"
+            validation={[required, isValidEmail]}
+            onValid={this.validEmail}
+            className="form-control"
+            placeholder="Enter email"
+            label="Email address"
+          />
+        </div>
+
+        <div className="form-group">
+          <Input
+            type="password"
+            validation={[required, isValidPassword]}
+            onValid={this.validPassword}
+            className="form-control"
+            placeholder="Enter password"
+            label="Password"
+          />
+        </div>
+
+        <div className="form-group">
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="customCheck1" />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="btn btn-primary btn-block"
+          disabled={!(validEmail && validPassword)}
+        >
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
 
 export default SignIn;
