f3e96374b1e32d5855fb7a7c0950927c063c68ef
lagbullet <lagbullet@gmail.com>
2020-07-31 12:29:53 +0300

    add logging for redux actions

--- src/components/redux/index.js
+++ src/components/redux/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './reducers/CardsReducer';
+import logger from '../loggger';
 
-const store = createStore(reducer);
+const store = createStore(reducer, applyMiddleware(thunk, logger));
 
 export default store;
